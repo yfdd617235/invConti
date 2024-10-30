6371f1cf638fa5ddb55acf6604a73be429b20a0e
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2024-10-29 21:42:58 -0500

    update

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -194,7 +194,7 @@ const HomaPage = () => {
         image: "ofi6.jpg",
       },
     ].map((card, index) => (
-      <div key={index} className="h-full shadow-md rounded-lg relative group overflow-hidden hover:scale-110">
+      <div key={index} className="h-full shadow-md rounded-lg relative group overflow-hidden hover:z-50">
         {/* Image */}
         <img
           src={`${import.meta.env.BASE_URL}${card.image}`}
