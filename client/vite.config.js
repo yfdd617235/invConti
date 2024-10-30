d5fd5b4064092ab1414bb46b6bd198d7693af632
yfdd617235 <yosefdavid91@gmail.com>
2024-10-30 08:18:25 -0500

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // base: '/', //Dominio personalizado
-  base: 'https://yfdd617235.github.io/invConti/'
+  // base: 'https://yfdd617235.github.io/invConti/'
+  base: '' //Dominio en netlify
 });
